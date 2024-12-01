5b0d54c029904720dcae1b1cf6031405ada15d37
IGATAJOHN <144548770+IGATAJOHN@users.noreply.github.com>
2024-12-01 18:08:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dath',
+    version='0.1',
+    install_requires=[
+        'spacy==3.7.2',
+    ],
+    scripts=[
+        'scripts/download_model.sh'
+    ]
+)
 
